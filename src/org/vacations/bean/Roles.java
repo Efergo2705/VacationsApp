5cf07dd540913d92ea2a18b272b78ef830ed3657
Efergo2705 <efgl2707@gmail.com>
2024-09-06 13:16:05 -0600

    Created branch ft/roles and added Roles.java in org.vacations.bean: Defined the Roles data model with id_rol and name_rol fields, including getters and setters.

--- src/org/vacations/bean/Roles.java
+++ src/org/vacations/bean/Roles.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.vacations.bean;
 
-/**
- *
- * @author ESTEBAN-GOMEZ
- */
 public class Roles {
+
+    private int id_rol;
+    private String name_rol;
+
+    public Roles() {
+    }
     
+    public Roles(int idRol, String nameRol){
+        this.id_rol = idRol;
+        this.name_rol = nameRol;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getName_rol() {
+        return name_rol;
+    }
+
+    public void setName_rol(String name_rol) {
+        this.name_rol = name_rol;
+    }
+    
+    
+
 }
