0fbd653ddf93ac0322074da3f2611ca6d7f03800
Efergo2705 <efgl2707@gmail.com>
2024-09-09 15:28:19 -0600

    Created branch ft/user and added User.java in org.vacations.bean: Defined the User data model with fields id_user, username, email, contrasena, and id_rol, including constructors, getters, and setters.

--- src/org/vacations/bean/User.java
+++ src/org/vacations/bean/User.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.vacations.bean;
 
-/**
- *
- * @author ESTEBAN-GOMEZ
- */
 public class User {
+    private int id_user;
+    private String username;
+    private String email;
+    private String contrasena;
+    private int id_rol;
+    
+    public User(){
+        
+    }
+    
+    public User(int idUser, String username, String email, String contrasena, int idRol){
+        this.id_user = idUser;
+        this.username = username;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.id_rol = idRol;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+    
     
 }
